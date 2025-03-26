8b71cdc926aee5611dda43125da8f1247c43e1e3
kspl456 <146167786+kspl456@users.noreply.github.com>
2025-03-26 20:34:13 +0530

    Update views.py

--- trivia_app/quiz/views.py
+++ trivia_app/quiz/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+def quiz_home(request):
+    return HttpResponse("<h1>Welcome to the Quiz!</h1>")
 # Create your views here.
