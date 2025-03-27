ecee9f45447c13379b35c3e4d9a35b15a670202d
kspl456 <146167786+kspl456@users.noreply.github.com>
2025-03-27 20:49:05 +0530

    updated urls and views

--- trivia_app/quiz/urls.py
+++ trivia_app/quiz/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.quiz_home, name='quiz_home'),
-    
+    path('fetch/', views.fetch_questions, name='fetch_questions'),
 ]
--- trivia_app/quiz/views.py
+++ trivia_app/quiz/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from django.http import JsonResponse
 def quiz_home(request):
     return HttpResponse("<h1>Welcome to the Quiz!</h1>")
 # Create your views here.
+def fetch_questions(request):
+    url = "https://opentdb.com/api.php?amount=10&type=multiple"
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data)
